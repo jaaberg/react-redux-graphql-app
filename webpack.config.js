cabd54eb930dc147e22f5ba53ae285c37626f1a8
jaaberg <aaberg89@gmail.com>
2015-10-10 12:11:35 +0200

    Add eslinter with AirBnB linting settings

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,13 @@ module.exports = {
       {test: /\.js$/, loader: 'react-hot-loader!babel-loader?stage=0', exclude: /node_modules/},
       {test: /\.scss$/, loader: 'style!css!autoprefixer-loader?browsers=last 2 versions!sass'},
       {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'} // inline base64 for <=8k images, direct URLs for the rest
+    ],
+    preLoaders: [
+      {test: /\.js$/, loader: "eslint-loader", exclude: /node_modules/}
     ]
+  },
+  eslint: {
+    configFile: '.eslintrc'
   },
   output: {
     path: path.join(__dirname, '/client/public/js'),
